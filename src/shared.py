71b6f803626baf25bd5d645a4d5350b6db53112a
Joseph Goulden <joegoulden@gmail.com>
2015-07-17 11:38:34 +0100

    Remove default known nodes and reduce PoW difficulty

--- src/shared.py
+++ src/shared.py
@@ -91,7 +91,7 @@ objectProcessorQueue = Queue.Queue(
 streamsInWhichIAmParticipating = {}
 
 #If changed, these values will cause particularly unexpected behavior: You won't be able to either send or receive messages because the proof of work you do (or demand) won't match that done or demanded by others. Don't change them!
-networkDefaultProofOfWorkNonceTrialsPerByte = 1000 #The amount of work that should be performed (and demanded) per byte of the payload.
+networkDefaultProofOfWorkNonceTrialsPerByte = 10 #The amount of work that should be performed (and demanded) per byte of the payload.
 networkDefaultPayloadLengthExtraBytes = 1000 #To make sending short messages a little more difficult, this value is added to the payload length for use in calculating the proof of work target.
 
 # Remember here the RPC port read from namecoin.conf so we can restore to
